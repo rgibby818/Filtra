1acc833f04d87f2e3551998dde79a23c02ddb538
Ryan Gibson <ryan.l.gibson@protonmail.com>
2025-08-04 13:00:53 -0400

    added a loading animation for Load More button in /history

--- client/src/components/HistoryPreview.vue
+++ client/src/components/HistoryPreview.vue
@@ -13,24 +13,28 @@
     <div
       v-for="(item, index) in images.originals"
       :key="index"
-      class="grid grid-cols-1 lg:grid-cols-2 gap-1 w-4/6"
+      class="grid grid-cols-1 gap-1 w-4/6"
     >
-      <time class="text-center dark:text-white">{{
-        convertEpochTime(item.fileName.split('-')[0])
-      }}</time>
-      <h3 class="text-center">
-        Filter Type:
-        {{ images.filtered[index].fileName.split('-').at(-1).split('.')[0].toUpperCase() }}
-      </h3>
+      <div class="flex flex-col sm:flex-row justify-between">
+        <time class="text-center dark:text-white font-mono text-xs sm:text-base">{{
+          convertEpochTime(item.fileName.split('-')[0])
+        }}</time>
+        <h3 class="text-center dark:text-white font-mono text-xs sm:text-base">
+          Filter:
+          {{ images.filtered[index].fileName.split('-').at(-1).split('.')[0].toUpperCase() }}
+        </h3>
+      </div>
+      <div class="flex flex-col md:flex-row md:gap-2 justify-around">
       <ImagePreview
-        class="lg:mr-2"
+        class="lg:mr-2 w-full"
         :image-url="`data:${item.mimetype};base64,${item.blob}`"
         alt-text="item.fileName"
       />
       <ImagePreview
-        class="lg:ml-2"
+        class="lg:ml-2 w-full"
         :image-url="`data:${images.filtered[index].mimetype};base64,${images.filtered[index].blob}`"
       />
+      </div>
     </div>
   </div>
   <div
@@ -40,8 +44,11 @@
     <h1 class="text-lg sm:text-2xl w-auto m-2 text-center">No images to show</h1>
     <DogImage />
   </div>
-  <div v-if="hasMore" class="flex justify-center mb-5">
-    <MyButton button-text="Load More" @click="getImages(start, end)" />
+  <div  class="mb-5">
+    <div class="relative flex justify-center w-full">
+    <MyButton :is-disabled="disableLoadMoreButton" :button-text="loadMoreButtonText" @click="getMoreImages(start, end)" />
+    <Loading v-if="loadingAnimationButton" width="w-7 absolute right-10 top-1" height="h-7" text-size="hidden"/>
+    </div>
   </div>
 </template>
 
@@ -54,14 +61,15 @@ import Loading from './Loading.vue'
 import MyButton from './MyButton.vue'
 
 const start = ref(0)
-const end = ref(10)
+const end = ref(5)
 
 const images = ref({})
-const originalImages = ref({})
-const filteredImages = ref({})
 const hasMore = ref(null)
+const disableLoadMoreButton = ref(false)
+const loadingAnimationButton = ref(false)
 const loading = ref(true)
 const errorMessage = ref(null)
+const loadMoreButtonText = ref('Load More')
 
 async function getImages(startIndex, endIndex) {
   try {
@@ -87,7 +95,6 @@ async function getImages(startIndex, endIndex) {
       images.value.filtered.push(...data.filtered)
       hasMore.value = data.hasMore
     }
-    console.log(images.value)
   } catch (error) {
     errorMessage.value = 'Network error'
     console.log('Fetch error:', error)
@@ -97,6 +104,19 @@ async function getImages(startIndex, endIndex) {
       start.value = end.value
       end.value = end.value + 10
     }
+  }
+}
+
+async function getMoreImages(start, end) {
+  disableLoadMoreButton.value = true
+  loadingAnimationButton.value = true
+  await getImages(start, end)
+  if (images.hasMore) {
+  disableLoadMoreButton.value = false
+  loadingAnimationButton.value = false
+  } else {
+    loadMoreButtonText.value = 'No More Images'
+    loadingAnimationButton.value = false
   }
 }
 
--- client/src/components/Loading.vue
+++ client/src/components/Loading.vue
@@ -1,8 +1,37 @@
 <template>
-  <div class="relative w-40 h-40 flex items-center justify-center">
+  <div :class="computedClasses">
     <div
       class="w-full h-full rounded-full absolute border-8 border-transparent border-t-slate-500 border-r-pink-500 border-b-blue-500 border-l-purple-500 animate-spin"
     ></div>
-    <span class="dark:text-white text-xl font-semibold animate-pulse">Loading...</span>
+    <span :class="textClasses" class="dark:text-white font-semibold animate-pulse">Loading...</span>
   </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  width: {
+    type: String,
+    default: "w-40"
+  },
+  height: {
+    type: String,
+    default: "h-40"
+  },
+  textSize: {
+    type: String,
+    default: "text-xl"
+  }
+
+})
+
+const computedClasses = computed(() => {
+  return ['relative', 'flex', 'items-center', 'justify-center', props.width, props.height]
+});
+
+const textClasses = computed(() => {
+  return ['dark:text-white', 'font-semibold', 'animate-pulse', props.textSize]
+})
+
+</script>
